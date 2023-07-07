fadc8fcd8aac5394187aa5df661a796871306cbc
luiseduardot17 <luis_eduardogdeo@hotmail.com>
2023-07-07 02:13:17 -0300

    ✨ feat: add propriedade de valor do veiculo

--- src/components/VehicleItem/VehicleItem.tsx
+++ src/components/VehicleItem/VehicleItem.tsx
@@ -1,19 +1,30 @@
-import React from 'react'
-import IVehicle from '../../interfaces/IVehicles';
+import React from "react";
+import IVehicle from "../../interfaces/IVehicles";
 
 interface IVehicleItemProps {
-    vehicle: IVehicle;
-  }
-  
-  const VehicleItem: React.FC<IVehicleItemProps> = ({ vehicle }) => {
-    return (
-      <div>
-        <h3>{vehicle.name}</h3>
-        <p>Modelo: {vehicle.model}</p>
-        <p>Fabricante: {vehicle.manufacturer}</p>
-        <p>Classe: {vehicle.vehicle_class}</p>
-      </div>
-    );
+  vehicle: IVehicle;
+}
+
+const VehicleItem: React.FC<IVehicleItemProps> = ({ vehicle }) => {
+  const formatValue = (value: string | number): string => {
+    if (value === "unknown") {
+      return "$ 20.000";
+    }
+
+    return `$ ${Number(value).toLocaleString()}`;
   };
 
-export default VehicleItem
+  return (
+    <div>
+      <h3>{vehicle.name}</h3>
+      <p>Modelo: {vehicle.model}</p>
+      <p>Fabricante: {vehicle.manufacturer}</p>
+      <p>Classe: {vehicle.vehicle_class}</p>
+      <div>
+        <p>Créditos Galácticos: {formatValue(vehicle.cost_in_credits)}</p>
+      </div>
+    </div>
+  );
+};
+
+export default VehicleItem;
