aadb9c046faa1b06588cc4b47c8853592dce3b0f
luiseduardot17 <luis_eduardogdeo@hotmail.com>
2023-07-06 22:54:14 -0300

    ✨ feat: add rotas as suas respectivas paginas

---
+++ src/routes/Routes.tsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Route, Routes as Switch } from "react-router-dom"
+import Home from "../pages/Home/Home"
+import Cart from "../pages/Cart/Cart"
+import Checkout from "../pages/Checkout/Checkout"
+import Success from "../pages/Success/Success"
+
+
+const Routes = () => {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" element={<Home />} />
+                <Route path="/cart" element={<Cart />} />
+                <Route path="/checkout" element={<Checkout />} />
+                <Route path="/success" element={<Success />} />
+
+            </Switch>
+        </BrowserRouter>
+    )
+}
+
+export default Routes
