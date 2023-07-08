afb80213ee0244bbd7b45d7ae727aaa9a8facd82
luiseduardot17 <luis_eduardogdeo@hotmail.com>
2023-07-08 02:28:16 -0300

    ✨ feat: agora é possivel ver o veiculo no carrinho

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -1,8 +1,27 @@
-
+import vehicleStore from "../../stores/VehicleStore";
+import { Link } from "react-router-dom";
 
 const Cart = () => {
+
+  const cartVehicles = vehicleStore.cartVehicles;
+
+
   return (
-    <div>Cart</div>
+    <div>
+      {cartVehicles.map((vehicle, index) => (
+        <div key={index}>
+          <h3>{vehicle.name}</h3>
+          <p>Valor: {vehicle.cost_in_credits}</p>
+
+          <div>
+            <button>
+              <Link to="/checkout">Ir para o Checkout</Link>
+            </button>
+          </div>
+        </div>
+      ))}
+
+    </div>
   )
 }
 
