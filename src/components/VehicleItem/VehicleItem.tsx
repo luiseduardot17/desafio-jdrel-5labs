0de3c1e9475a518b3a3fa8163b81a4530f7180d5
luiseduardot17 <luis_eduardogdeo@hotmail.com>
2023-07-08 00:21:03 -0300

    ✨ feat: funçao que pega o id da url e link para detalhes

--- src/components/VehicleItem/VehicleItem.tsx
+++ src/components/VehicleItem/VehicleItem.tsx
@@ -1,19 +1,28 @@
 import React from "react";
 import IVehicle from "../../interfaces/IVehicles";
+import { Link } from "react-router-dom"
 
 interface IVehicleItemProps {
   vehicle: IVehicle;
 }
 
 const VehicleItem: React.FC<IVehicleItemProps> = ({ vehicle }) => {
+  const extractIdFromUrl = (url: string): string => {
+    const parts = url.split("/");  // aqui eu extraio o id do final da url do item dividindo em partes
+    const id = parts[5]
+    return id || "";
+    
+  };
+  
+  const id = extractIdFromUrl(vehicle.url)
+  
   const formatValue = (value: string | number): string => {
     if (value === "unknown") {
       return "$ 20.000";
     }
-
     return `$ ${Number(value).toLocaleString()}`;
   };
-
+  
   return (
     <div>
       <h3>{vehicle.name}</h3>
@@ -23,6 +32,8 @@ const VehicleItem: React.FC<IVehicleItemProps> = ({ vehicle }) => {
       <div>
         <p>Créditos Galácticos: {formatValue(vehicle.cost_in_credits)}</p>
       </div>
+      <Link to={`/vehicles/${id}`}>Ver detalhes</Link>
+      
     </div>
   );
 };
