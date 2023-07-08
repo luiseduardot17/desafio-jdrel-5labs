f3fc08f36922039f312e864955f00c14914b49e2
luiseduardot17 <luis_eduardogdeo@hotmail.com>
2023-07-08 15:49:44 -0300

    ✨ feat: botao que remove o item do carrinho

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -1,28 +1,43 @@
 import vehicleStore from "../../stores/VehicleStore";
 import { Link } from "react-router-dom";
+import { RiDeleteBin6Line } from 'react-icons/ri';
+import { observer } from "mobx-react-lite";
+import { formatValue } from "../../utils/utils";
 
 const Cart = () => {
+  const { cartVehicles } = vehicleStore;
 
-  const cartVehicles = vehicleStore.cartVehicles;
-
+  const handleRemoveFromCart = (vehicleId: string) => {
+    vehicleStore.removeFromCart(vehicleId);
+  };
 
   return (
     <div>
-      {cartVehicles.map((vehicle, index) => (
-        <div key={index}>
+      {cartVehicles.map((vehicle) => (
+        <div key={vehicle.id}>
           <h3>{vehicle.name}</h3>
-          <p>Valor: {vehicle.cost_in_credits}</p>
-
-          <div>
-            <button>
-              <Link to="/checkout">Ir para o Checkout</Link>
-            </button>
-          </div>
+          <p>Valor: {formatValue(vehicle.cost_in_credits)}</p>
+          <button onClick={() => handleRemoveFromCart(vehicle.id)}>
+            <RiDeleteBin6Line />
+          </button>
         </div>
       ))}
 
+      {cartVehicles.length > 0 && (
+        <div>
+          <button>
+            <Link to="/checkout">Ir para o Checkout</Link>
+          </button>
+        </div>
+      )}
+
+      <div>
+        <button>
+          <Link to="/">Voltar à lista de veículos</Link>
+        </button>
+      </div>
     </div>
   )
 }
 
-export default Cart
+export default observer(Cart)
