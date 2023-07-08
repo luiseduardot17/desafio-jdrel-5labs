b0949b25b97d6ee7f0399ecc9b664fd698c08a3c
luiseduardot17 <luis_eduardogdeo@hotmail.com>
2023-07-08 17:30:00 -0300

    ✨ feat: botao que volta ao top da pagina

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import { Link } from 'react-scroll';
 import VehicleList from '../../components/VehicleList/VehicleList'
+import { FaCircleArrowUp } from 'react-icons/fa6';
 
 const Home = () => {
   return (
     <div>
       <VehicleList/>
+      <Link to="top" smooth={true} duration={500}>
+        <button><FaCircleArrowUp/> Voltar ao topo</button>
+      </Link>
     </div>
   )
 }
