bb8f40e6bd6251007cbdcd2ac6c497aea46b291c
luiseduardot17 <luis_eduardogdeo@hotmail.com>
2023-07-09 01:43:37 -0300

    ✨ feat: tela de sucesso da compra

--- src/pages/Success/Success.tsx
+++ src/pages/Success/Success.tsx
@@ -1,8 +1,16 @@
-
+import { Link } from "react-router-dom"
 
 const Success = () => {
   return (
-    <div>Success</div>
+    <div>
+      <h3>Compra realizada com sucesso!</h3>
+      <span>O prazo de entrega começa a contar após a confirmação do pagamento.</span>
+      <div>
+        <Link to="/">
+          <button>CONTINUAR COMPRANDO</button>
+        </Link>
+      </div>
+    </div>
   )
 }
 
