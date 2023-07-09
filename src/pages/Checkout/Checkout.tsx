fca6de961e9ed4b19787b241472eca09e255e4b6
luiseduardot17 <luis_eduardogdeo@hotmail.com>
2023-07-09 00:16:10 -0300

    ✨ feat: importado formulario de checkout

--- src/pages/Checkout/Checkout.tsx
+++ src/pages/Checkout/Checkout.tsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import CheckoutForm from '../../components/CheckoutForm/CheckoutForm';
+import { useNavigate } from 'react-router-dom';
 
 const Checkout = () => {
+  const navigate = useNavigate();
+
   return (
-    <div>Checkout</div>
+    <div>
+      <div>
+        <h3>Antes de finalizar seu pedido, vamos precisar dos seus dados:</h3>
+      </div>
+      <CheckoutForm navigate={navigate}/>
+    </div>
   )
 }
 
