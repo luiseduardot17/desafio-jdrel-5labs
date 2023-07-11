7eeaa1022195ea6d84d86f404ef2c02f14dbbc0c
luiseduardot17 <luis_eduardogdeo@hotmail.com>
2023-07-11 14:31:37 -0300

    ✨ feat: container com resumo do pedido atualizado

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 import { RiDeleteBin6Line } from 'react-icons/ri';
 import { observer } from "mobx-react-lite";
 import { formatValue } from "../../utils/utils";
+import { BsCart3 } from "react-icons/bs";
+import style from './Cart.module.css'
 
 const Cart = () => {
   const { cartVehicles } = vehicleStore;
@@ -12,36 +14,54 @@ const Cart = () => {
   };
 
   return (
-    <div>
-      {cartVehicles.map((vehicle) => (
-        <div key={vehicle.id}>
-          <h3>{vehicle.name}</h3>
-          <p>Valor: {formatValue(vehicle.cost_in_credits)}</p>
-          <button onClick={() => handleRemoveFromCart(vehicle.id)}>
-            <RiDeleteBin6Line /> REMOVER
-          </button>
-        </div>
-      ))}
+    <div className={style.Container}>
+      <div className={style.Order}>
+        <div className={style.ellipse}></div>
+        {cartVehicles.length > 0 && (
+          <div className={style.Descritivo}>
+            <h3>Veículo</h3>
+            <h3>Créditos Galácticos</h3>
+            <h3>Ação</h3>
+          </div>
+        )}
+        {cartVehicles.map((vehicle) => (
+          <div key={vehicle.id} className={style.Produto}>
 
-      {cartVehicles.length > 0 && (
+            <div className={style.Item}>
+              <div><h3>{vehicle.name}</h3></div>
+              <div>
+                <span>{formatValue(vehicle.cost_in_credits)}</span>
+              </div>
+              <div className={style.ContainerButton}><button onClick={() => handleRemoveFromCart(vehicle.id)}>
+                <RiDeleteBin6Line /> REMOVER
+              </button></div>
+            </div>
+          </div>
+        ))}
+
+        {cartVehicles.length > 0 && (
+          <div>
+            <Link to="/checkout">
+              <button className={style.ButtonFecharPedido}>FECHAR PEDIDO</button>
+            </Link>
+          </div>
+        )}
+
+        {cartVehicles.length == 0 && (
+          <div>
+            <div className={style.TextoVazio}>
+              <b>O seu carrinho está vazio. </b>
+              <span>Deseja olhar outros veículos similares?</span>
+            </div>
+            <div className={style.image}></div>
+          </div>
+        )}
+
         <div>
-          <Link to="/checkout">
-            <button>FECHAR PEDIDO</button>
+          <Link to="/vehicles">
+            <button className={style.ButtonContinuar}><BsCart3 /> CONTINUAR COMPRANDO</button>
           </Link>
         </div>
-      )}
-
-      {cartVehicles.length == 0 && (
-        <div>
-          <b>O seu carrinho está vazio.</b>
-          <span>Deseja olhar outros veículos similares?</span>
-        </div>
-      )}
-
-      <div>
-        <Link to="/">
-          <button>CONTINUAR COMPRANDO</button>
-        </Link>
       </div>
     </div>
   )
