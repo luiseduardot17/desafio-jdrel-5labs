343c94b762bd28a25c680730b5c7f9119bafc116
luiseduardot17 <luis_eduardogdeo@hotmail.com>
2023-07-11 10:42:16 -0300

    ✨ feat: add classes para estilizaçao

--- src/components/VehicleItem/VehicleItem.tsx
+++ src/components/VehicleItem/VehicleItem.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import IVehicle from "../../interfaces/IVehicles";
 import { Link } from "react-router-dom"
+import style from "./VehicleItem.module.css"
+import { formatValue } from "../../utils/utils";
 
 interface IVehicleItemProps {
   vehicle: IVehicle;
@@ -11,29 +13,32 @@ const VehicleItem: React.FC<IVehicleItemProps> = ({ vehicle }) => {
     const parts = url.split("/");  // aqui eu extraio o id do final da url do item dividindo em partes
     const id = parts[5]
     return id || "";
-    
+
   };
-  
+
   const id = extractIdFromUrl(vehicle.url)
-  
-  const formatValue = (value: string | number): string => {
-    if (value === "unknown") {
-      return "$ 20.000";
-    }
-    return `$ ${Number(value).toLocaleString()}`;
-  };
-  
+
   return (
-    <div>
-      <h3>{vehicle.name}</h3>
-      <p>Modelo: {vehicle.model}</p>
-      <p>Fabricante: {vehicle.manufacturer}</p>
-      <p>Classe: {vehicle.vehicle_class}</p>
-      <div>
-        <p>Créditos Galácticos: {formatValue(vehicle.cost_in_credits)}</p>
+    <div className={style.vehicleCard}>
+      <div className={style.Container}>
+            <div className={style.vehicleImage}>
+              IMG
+            </div>
+            <div className={style.vehicleInfos}>
+              <h3>{vehicle.name}</h3>
+              <p>Modelo: {vehicle.model}</p>
+              <p>Fabricante: {vehicle.manufacturer}</p>
+              <p>Classe: {vehicle.vehicle_class}</p>
+              <div>
+                <span>Créditos Galácticos: {formatValue(vehicle.cost_in_credits)}</span>
+              </div>
+            </div>
       </div>
-      <Link to={`/vehicles/${id}`}>Ver detalhes</Link>
-      
+      <div className={style.Link}>
+        <Link to={`/vehicles/${id}`}>
+          <button>Ver detalhes</button>
+        </Link>
+      </div>
     </div>
   );
 };
