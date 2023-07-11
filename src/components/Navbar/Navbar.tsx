6b26f7b865dd972824ca5db6faf4445bdfc3099e
luiseduardot17 <luis_eduardogdeo@hotmail.com>
2023-07-11 10:38:41 -0300

    ✨ feat: criada navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,13 +1,24 @@
 import { Link } from 'react-router-dom';
 import { RiShoppingCartLine } from 'react-icons/ri';
+import style from './Navbar.module.css'
 
 const Navbar = () => {
     return (
         <header>
-            <h1>Star Wars Vehicle Explorer</h1>
-            <Link to="/cart">
-                <RiShoppingCartLine />
-            </Link>
+            <div className={style.ellipse}></div>
+            <div className={style.navbar}>
+                <div className={style.image}></div>
+                <nav>
+                    <a href='/'>Início</a>
+                    <a>Veículos</a>
+                    <a>Sobre</a>
+                </nav>
+                <div className={style.divButton}>
+                    <Link to="/cart">
+                        <button className={style.buttonCart}><RiShoppingCartLine /> Carrinho</button>
+                    </Link>
+                </div>
+            </div>
         </header>
     );
 };
