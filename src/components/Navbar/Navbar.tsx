8be36fd4d6638f6e337713469e6dcf5900c45079
luiseduardot17 <luis_eduardogdeo@hotmail.com>
2023-07-12 02:43:02 -0300

    ✨ feat: agora o componente tem botao de hamburguer

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,26 +1,77 @@
 import { Link } from 'react-router-dom';
-import { BsCart3 } from 'react-icons/bs';
+import { useState, useEffect } from 'react';
+import { BsCart3, BsXLg } from 'react-icons/bs';
+import { GiHamburgerMenu } from 'react-icons/gi'
 import style from './Navbar.module.css'
 
 const Navbar = () => {
-    return (
-        <header>
-            <div className={style.ellipse}></div>
-            <div className={style.navbar}>
-                <div className={style.image}></div>
-                <nav>
-                    <Link to='/'>Início</Link>
-                    <Link to='/vehicles'>Veículos</Link>
-                    <Link to='/'>Sobre</Link>
-                </nav>
-                <div className={style.divButton}>
-                    <Link to="/cart">
-                        <button className={style.buttonCart}><BsCart3/> Carrinho</button>
-                    </Link>
-                </div>
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  return (
+    <header>
+      <div className={style.ellipse}></div>
+      <div className={style.navbar}>
+        <div className={style.image}></div>
+        {windowWidth <= 425 ? (
+          <>
+            <span className={`${style.hamburgerButton} ${isMenuOpen ? style.open : ''}`} onClick={toggleMenu}>
+              {isMenuOpen ? <BsXLg size={30}/> : <GiHamburgerMenu size={30}/>}
+            </span>
+            {isMenuOpen && (
+              <nav className={style.menuOpen}>
+                <Link to="/" onClick={toggleMenu}>
+                  Início
+                </Link>
+                <Link to="/vehicles" onClick={toggleMenu}>
+                  Veículos
+                </Link>
+                <Link to="/" onClick={toggleMenu}>
+                  Sobre
+                </Link>
+                <Link to="/cart">
+                  <button className={style.buttonCart}>
+                    <BsCart3 /> Carrinho
+                  </button>
+                </Link>
+              </nav>
+            )}
+          </>
+        ) : (
+          <>
+            <nav className={style.nav}>
+              <Link to="/">Início</Link>
+              <Link to="/vehicles">Veículos</Link>
+              <Link to="/">Sobre</Link>
+            </nav>
+            <div className={style.divButton}>
+              <Link to="/cart">
+                <button className={style.buttonCart}>
+                  <BsCart3 /> Carrinho
+                </button>
+              </Link>
             </div>
-        </header>
-    );
+          </>
+        )}
+      </div>
+    </header>
+  );
 };
 
 export default Navbar;
