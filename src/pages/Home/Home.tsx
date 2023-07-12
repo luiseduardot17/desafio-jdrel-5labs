f3c2a8959659bdc6f8597fc7846af76e1ebf50f6
luiseduardot17 <luis_eduardogdeo@hotmail.com>
2023-07-12 17:22:11 -0300

    ✨ feat: novos conteudos a pagina inicial

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -8,11 +8,27 @@ const Home = () => {
       <section>
         <h1>Star Wars Vehicle Explorer</h1>
         <div className={style.paragrafo}>
-          <p>Se você é um verdadeiro fã de Star Wars e sempre sonhou em pilotar uma das icônicas naves ou veículos que habitam essa galáxia distante, você está no lugar certo. 
+          <p>Se você é um verdadeiro fã de Star Wars e sempre sonhou em pilotar uma das icônicas naves ou veículos que habitam essa galáxia distante, você está no lugar certo.
             Aqui, você encontrará uma vasta seleção de veículos incríveis, prontos para transportá-lo para aventuras intergalácticas.</p>
         </div>
         <div className={style.imageAnimation}></div>
         <div className={style.ellipse2}></div>
+
+        <div className={style.paragrafo}>
+          <p>Explore nossa coleção de naves espaciais lendárias, como a Millennium Falcon e o X-wing, ou mergulhe nas areias de Tatooine com os famosos speeders.
+            Cada veículo é cuidadosamente selecionado para oferecer a você a experiência mais autêntica e emocionante possível.</p>
+        </div>
+        <div className={style.imageContainer2}>
+          <div className={style.imageAnimation2}></div>
+        </div>
+
+        <div className={style.paragrafo}>
+          <p>Nossos veículos são mais do que meras máquinas - eles são símbolos de poder, liberdade e heroísmo. Ao adquirir um desses veículos, você estará se conectando diretamente
+            com a incrível história e mitologia de Star Wars.</p>
+        </div>
+        <div className={style.imageContainer2}>
+          <div className={style.imageAnimation3}></div>
+        </div>
       </section>
       <div className={style.topButton}>
         <Link to="top" smooth={true} duration={500}>
