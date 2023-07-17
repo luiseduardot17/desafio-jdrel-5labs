620e7bf68ea17971b45bd26c6d88a2e2c2364ac0
luiseduardot17 <luis_eduardogdeo@hotmail.com>
2023-07-17 19:59:43 -0300

    👌 style: add classes para estilizaçao

--- src/components/SuccessScreen/SuccessScreen.tsx
+++ src/components/SuccessScreen/SuccessScreen.tsx
@@ -7,27 +7,31 @@ const SuccessScreen = () => {
     function generateOrder() {
         let randomNumbers = '';
         for (let i = 0; i < 6; i++) {
-          const randomNumber = Math.floor(Math.random() * 10);
-          randomNumbers += randomNumber;
+            const randomNumber = Math.floor(Math.random() * 10);
+            randomNumbers += randomNumber;
         }
         return randomNumbers;
-      }
-      
-      const sixNumbers = generateOrder();
+    }
+
+    const sixNumbers = generateOrder();
 
     return (
-        <div className={style.containerOrder}>
-            <h3><BsFillCheckCircleFill /> Compra realizada com sucesso!</h3>
-            <h3>Número do pedido: {sixNumbers}</h3>
-            <div>
-                <span>Após a confirmação do pagamento nossa equipe entrará em contato para retirada de seu novo veículo.</span>
+        <section className={style.containerOrder}>
+            <div className={style.ellipse}></div>
+            <div className={style.pedido}>
+                <h3><BsFillCheckCircleFill className={style.icone} /> Compra realizada com sucesso!</h3>
+                <h3>Número do pedido: {sixNumbers}</h3>
+                <div>
+                    <span>Após a confirmação do pagamento nossa equipe entrará em contato para retirada de seu novo veículo.</span>
+                </div>
+                <div>
+                    <Link to="/">
+                        <button className={style.ButtonInicio}>VOLTAR AO INÍCIO</button>
+                    </Link>
+                </div>
             </div>
-            <div>
-                <Link to="/">
-                    <button>VOLTAR AO INÍCIO</button>
-                </Link>
-            </div>
-        </div>
+
+        </section>
     )
 }
 
