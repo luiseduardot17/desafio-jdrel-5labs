fd08d569df427dba3844d5aa87f0ffe9ed180dce
luiseduardot17 <luis_eduardogdeo@hotmail.com>
2023-07-17 11:24:50 -0300

    ✨ feat: criada funçao que gera o numero do pedido

--- src/components/SuccessScreen/SuccessScreen.tsx
+++ src/components/SuccessScreen/SuccessScreen.tsx
@@ -1,16 +1,30 @@
 import { Link } from "react-router-dom"
 import { BsFillCheckCircleFill } from "react-icons/bs";
+import style from './SuccessScreen.module.css';
 
 const SuccessScreen = () => {
+
+    function generateOrder() {
+        let randomNumbers = '';
+        for (let i = 0; i < 6; i++) {
+          const randomNumber = Math.floor(Math.random() * 10);
+          randomNumbers += randomNumber;
+        }
+        return randomNumbers;
+      }
+      
+      const sixNumbers = generateOrder();
+
     return (
-        <div>
+        <div className={style.containerOrder}>
             <h3><BsFillCheckCircleFill /> Compra realizada com sucesso!</h3>
+            <h3>Número do pedido: {sixNumbers}</h3>
             <div>
-                <span>O prazo de entrega começa a contar após a confirmação do pagamento.</span>
+                <span>Após a confirmação do pagamento nossa equipe entrará em contato para retirada de seu novo veículo.</span>
             </div>
             <div>
                 <Link to="/">
-                    <button>CONTINUAR COMPRANDO</button>
+                    <button>VOLTAR AO INÍCIO</button>
                 </Link>
             </div>
         </div>
